d44dabcaf33d58b568a790760f3c4be2adc6acb4
German Renato Cobian <germancobian@hotmail.com>
2023-03-03 17:30:59 -0800

    Create devise User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :purchases, dependent: :destroy
+
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 end
