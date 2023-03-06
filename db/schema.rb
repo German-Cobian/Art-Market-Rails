6f50e055087db8ff1597ca94421f41cf9d117053
German Renato Cobian <germancobian@hotmail.com>
2023-03-06 13:55:53 -0800

    Include default role into User model

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_200101) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.integer "role"
+    t.integer "role", default: 0
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
